62bf884fd17d4e66eecdd7085956fbff63079f6d
Nicholas Gamolin <asr8gn@virginia.edu>
2025-04-23 01:38:05 -0400

    added enemies, enemy behavior, lock and key mechanics, and additional sprites

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject stationList;
+
+    int stationIndex;
+    int numStations;
+
+    private enum State {looking, walking, concerned};
+    private State state;
+
+    // used for moving the character and handling physics
+    private Rigidbody2D rigidBody;
+
+    // set the enemy speed
+    [SerializeField] private float speed = 1f;
+
+    // for player flashlight detection
+    public GameObject playerObject;
+    private Vector2 lastPlayerPosition;
+    private bool inPlayerVisionCone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        stationIndex = 0;
+        numStations = stationList.transform.childCount;
+        Debug.Log(numStations.ToString());
+
+        state = State.walking;
+
+        inPlayerVisionCone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 stationPosition = stationList.transform.GetChild(stationIndex).position;
+
+        Vector2 stationDirection = new Vector2(stationPosition.x - transform.position.x, stationPosition.y - transform.position.y);
+
+        if (inPlayerVisionCone) {
+            Vector2 currentlayerPosition = playerObject.transform.position;
+            Vector2 playerDirection = new Vector2(currentlayerPosition.x - transform.position.x, currentlayerPosition.y - transform.position.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection, playerDirection.magnitude, LayerMask.GetMask("Wall"));
+
+            if (hit) {
+                // wall in the way, dont reload scene
+                Debug.Log("detected hit");
+            }
+            else {
+                Debug.Log("no wall detected");
+                state = State.concerned;
+                lastPlayerPosition = currentlayerPosition;
+            }
+        }
+
+        if (state == State.walking) {
+
+            if (stationDirection.magnitude > 0.3f) {
+                rigidBody.velocity = speed * stationDirection / stationDirection.magnitude;
+
+                //Debug.Log(Quaternion.LookRotation(stationDirection).ToString());
+                
+                // float angle = Mathf.Atan2(stationDirection.y, stationDirection.x) * Mathf.Rad2Deg;
+        	    // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            }
+            else {
+                state = State.looking;
+
+                stationIndex++;
+                if (stationIndex == numStations) {
+                    stationIndex = 0;
+                }
+
+                StartCoroutine(idle());
+            }
+
+            float angle = Mathf.Atan2(stationPosition.y - transform.position.y, stationPosition.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
+            
+        }
+        else if (state == State.looking) {
+            float angle = Mathf.Atan2(stationPosition.y - transform.position.y, stationPosition.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
+        }
+        else if (state == State.concerned) {
+
+            StartCoroutine(lookForPlayer());
+
+            float angle = Mathf.Atan2(lastPlayerPosition.y - transform.position.y, lastPlayerPosition.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "PlayerVisionCone") {
+            
+            // lastPlayerPosition = playerObject.transform.position;
+            // Vector2 playerDirection = new Vector2(lastPlayerPosition.x - transform.position.x, lastPlayerPosition.y - transform.position.y);
+
+            // RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection, playerDirection.magnitude, LayerMask.GetMask("Wall"));
+
+            // if (hit) {
+            //     // wall in the way, dont reload scene
+            //     Debug.Log("detected hit");
+            // }
+            // else {
+            //     Debug.Log("no wall detected");
+            //     state = State.concerned;
+            // }
+            inPlayerVisionCone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "PlayerVisionCone") {
+            inPlayerVisionCone = false;
+        }
+    }
+
+    IEnumerator idle() {
+        yield return new WaitForSeconds(5f);
+        
+        if (state != State.concerned) {
+            state = State.walking;
+        }    
+    }
+
+    IEnumerator lookForPlayer() {
+        yield return new WaitForSeconds(5f);
+        
+        state = State.walking;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,10 +12,25 @@ public class PlayerController : MonoBehaviour
     // set the player speed
     [SerializeField] private float speed = 5f;
 
+    // keep track of key collection
+    private bool hasKey;
+
+    // handle player text
+    public GameObject playerTextObject;
+    private TextMeshPro playerText;
+
+    // flashlight controls
+    private bool flashlight;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        hasKey = false;
+
+        playerText = playerTextObject.GetComponent<TextMeshPro>();
+
+        flashlight = true;
     }
 
     // Update is called once per frame
@@ -44,6 +61,57 @@ public class PlayerController : MonoBehaviour
         Vector2 mouseDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
 
         transform.right = mouseDirection;
+
+    }
+
+    // handling collision with guard vision cones
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "VisionCone") {
+            
+            // string currentSceneName = SceneManager.GetActiveScene().name;
+            // SceneManager.LoadScene(currentSceneName);
+
+            GameObject guardCollidedWith = collision.transform.parent.gameObject;
+            Vector2 guardDirection = new Vector2(guardCollidedWith.transform.position.x - transform.position.x, guardCollidedWith.transform.position.y - transform.position.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, guardDirection, guardDirection.magnitude, LayerMask.GetMask("Wall"));
+
+
+            // Debug.Log(LayerMask.GetMask("Wall").ToString());
+            // Debug.Log(guardDirection.magnitude.ToString());
+            // Debug.DrawRay(transform.position, guardDirection, Color.green, 60f);
+
+            if (hit) {
+                // wall in the way, dont reload scene
+                Debug.Log("detected hit");
+            }
+            else {
+                Debug.Log("no wall detected");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            
+        }
+        else if (collision.tag == "Key") {
+            hasKey = true;
+            playerText.text = "Found a key!";
+            Destroy(collision.gameObject);
+            StartCoroutine(clearText());
+        }
+        else if (collision.tag == "Door") {
+            // load next scene, currently just restart scene
+            if (hasKey) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else {
+                playerText.text = "Need key...";
+                StartCoroutine(clearText());
+            }
+        }
+    }
+
+    IEnumerator clearText() {
+        yield return new WaitForSeconds(5f);
+        playerText.text = "";
     }
 }
 
